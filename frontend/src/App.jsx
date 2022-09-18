d3c7c855899752a9601dd48ecaa707035e1927bb
Jamajal <leandroliberiomachado@hotmail.com>
2022-09-18 02:04:05 -0300

    fixing swith again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { Switch, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Cataloguer from './pages/Cataloguer'
 import Login from './pages/Login'
 import RegistrateCard from './pages/RegistrateCard'
 
-
 function App() {
   return (
-      <Switch>
+      <Routes>
         <Route exact path="/" element={<Cataloguer />}></Route>
         <Route exact path="/login" element={<Login />}></Route>
         <Route exact path="/new-card" element={<RegistrateCard />}></Route>
-      </Switch>
+      </Routes>
   )
 }
 
