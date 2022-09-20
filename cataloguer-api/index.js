67f01dff5c3983eebbdb5a3d689f61708304ac08
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2022-09-20 18:51:37 -0300

    Use https

--- cataloguer-api/index.js
+++ cataloguer-api/index.js
@@ -3,6 +3,12 @@ const ApiRoutes = require('./src/http/routes/Routes')
 const express = require('express')
 const cors = require('cors')
 const app = express();
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+    if (req.secure) //Se a requisição feita é segura (é HTTPS)
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+    else //Se a requisição não for segura (é HTTP)
+        res.redirect(`https://${req.hostname}${req.url}`); //Redireciona a requisição para o mesmo host e url mas com HTTPS e termina a request
+});
 app.use(cors())
 async function main() {
     const routes = new ApiRoutes()
