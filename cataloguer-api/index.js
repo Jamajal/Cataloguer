333078b4bd7f3c23818744015093ee1fba488020
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2022-09-20 13:50:29 -0300

    revert port

--- cataloguer-api/index.js
+++ cataloguer-api/index.js
@@ -19,8 +19,8 @@ async function main() {
         const result = await routes.getGeneral(req)
         res.send(result)
     })
-    app.listen(80, () =>
-        console.log(`Example app listening on port ${80}!`),
+    app.listen(5000, () =>
+        console.log(`Example app listening on port ${5000}!`),
     );
 }
 main()
